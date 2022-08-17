f620cfdfe607bb6caf2534d78dcb99c09ff25cfe
Dmitriy <dmt.beloff@yandex.ru>
2022-08-17 21:53:02 +0300

    Собирается, запускается с Vue.

--- hw10/src/main/java/ru/otus/spring/belov/controller/BookCommentController.java
+++ hw10/src/main/java/ru/otus/spring/belov/controller/BookCommentController.java
@@ -11,7 +11,7 @@ import ru.otus.spring.belov.service.BookService;
 /**
  * Контроллер по работе с комментариями
  */
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class BookCommentController {
 
@@ -20,30 +20,30 @@ public class BookCommentController {
     /** Сервис по работе с книгами */
     private final BookService bookService;
 
-    /**
-     * Возвращает форму добавления комментария
-     * @param bookId идентификатор книги
-     * @param model модель данных
-     * @return форма добавления комментария
-     */
-    @GetMapping
-    @RequestMapping("book/{bookId}/comment/new")
-    public String newComment(@PathVariable Long bookId, Model model) {
-        bookService.getById(bookId);
-        model.addAttribute("bookId", bookId);
-        model.addAttribute("comment", new BookComment());
-        return "book/comment/form";
-    }
-
-    /**
-     * Сохраняет комментарий
-     * @param bookId идентификатор книги
-     * @param comment комментарий
-     * @return форма просмотра книги
-     */
-    @PostMapping("book/{bookId}/comment")
-    public String save(@ModelAttribute @PathVariable Long bookId, BookComment comment) {
-        commentService.save(comment, bookId);
-        return "redirect:/book/" + bookId + "/view";
-    }
+//    /**
+//     * Возвращает форму добавления комментария
+//     * @param bookId идентификатор книги
+//     * @param model модель данных
+//     * @return форма добавления комментария
+//     */
+//    @GetMapping
+//    @RequestMapping("book/{bookId}/comment/new")
+//    public String newComment(@PathVariable Long bookId, Model model) {
+//        bookService.getById(bookId);
+//        model.addAttribute("bookId", bookId);
+//        model.addAttribute("comment", new BookComment());
+//        return "book/comment/form";
+//    }
+//
+//    /**
+//     * Сохраняет комментарий
+//     * @param bookId идентификатор книги
+//     * @param comment комментарий
+//     * @return форма просмотра книги
+//     */
+//    @PostMapping("book/{bookId}/comment")
+//    public String save(@ModelAttribute @PathVariable Long bookId, BookComment comment) {
+//        commentService.save(comment, bookId);
+//        return "redirect:/book/" + bookId + "/view";
+//    }
 }
