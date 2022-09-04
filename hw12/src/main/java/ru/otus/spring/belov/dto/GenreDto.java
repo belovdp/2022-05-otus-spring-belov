1b1bfe851c57b6f9ef76c107a2bd999835718472
Dmitriy <dmt.beloff@yandex.ru>
2022-09-04 14:34:29 +0300

    Основа для hw12

---
+++ hw12/src/main/java/ru/otus/spring/belov/dto/GenreDto.java
@@ -0,0 +1,24 @@
+package ru.otus.spring.belov.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Жанр
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@ToString
+@Getter
+public class GenreDto {
+
+    /** Идентификатор */
+    @NotNull(message = "Жанр должен быть выбран")
+    private Long id;
+
+    /** Название жанра */
+    private String name;
+}
---
+++ hw12/src/main/java/ru/otus/spring/belov/service/GenreServiceImpl.java
@@ -0,0 +1,27 @@
+package ru.otus.spring.belov.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.spring.belov.dto.GenreDto;
+import ru.otus.spring.belov.dto.mappers.GenreMapper;
+import ru.otus.spring.belov.repositories.GenreRepository;
+
+import java.util.List;
+
+/**
+ * Сервис по работе с жанрами
+ */
+@Service
+@RequiredArgsConstructor
+public class GenreServiceImpl implements GenreService {
+
+    /** Репозиторий по работе с жанрами */
+    private final GenreRepository genreRepository;
+    /** Преобразователь сущностей в DTO */
+    private final GenreMapper mapper;
+
+    @Override
+    public List<GenreDto> getAll() {
+        return mapper.toDto(genreRepository.findAll());
+    }
+}
